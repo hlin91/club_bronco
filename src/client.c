41d054e82a95dad81d8743df03887dd43a2cffcd
Lawrence <lchua2314@gmail.com>
2020-11-05 22:31:11 -0800

    Fixed spacing to 4 space indentations

--- src/client.c
+++ src/client.c
@@ -17,10 +17,10 @@ int main()
     inet_pton(AF_INET, "127.0.0.1", &server.sin_addr); //This binds the client to localhost
     connect(fd, (struct sockaddr *)&server, sizeof(server)); //This connects the client to the server.
     while(1) {
-    printf("Enter a message: ");
-    fgets(message, 100, stdin);
-    send(fd, message, strlen(message), 0);
-    //An extra breaking condition can be added here (to terminate the while loop)
+        printf("Enter a message: ");
+        fgets(message, 100, stdin);
+        send(fd, message, strlen(message), 0);
+        //An extra breaking condition can be added here (to terminate the while loop)
     }
     return 0;
 }
