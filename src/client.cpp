70acef340ce2cd812873e690fd77e5386bf5eb76
Nick <nicholastcalkins@gmail.com>
2020-11-07 00:38:24 -0800

    Using functions

--- src/client.cpp
+++ src/client.cpp
@@ -8,41 +8,56 @@
 
 using namespace std;
 
-int main() 
-{
-    struct sockaddr_in server;
-    int fd;
+int create_server_socket(int port) {
 
-    int connection;
+    struct sockaddr_in server_address;
 
-    int portNum;
+    int opt = 1;
 
-    char message[1024];
+    bzero(&server_address, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(port);
     
-    // Populate socket address structure
-    server.sin_family = AF_INET;
-    server.sin_port = htons(portNum);
-    //server.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    //Create the socket
+    int sock = socket(AF_INET,
+                        SOCK_STREAM,
+                        0);
 
-    // Attempt to create a socket and assign it to fd
-    if (fd = socket(AF_INET, SOCK_STREAM, 0) < 0) {
+    //Check for error with socket
+    if (sock < 0) {
         perror("Error with socket");
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
-    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr); //This binds the client to localhost
-    if (connect(fd, (struct sockaddr *) &server, sizeof(server)) == -1) {
-        cout << "connection failed\n";
-        perror("Connection failed");
-        return -1;
-    } //This connects the client to the server.
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    }
+
+    // Attempt to make the socket (fd) a listening type socket
+    if (listen(sock, 10) <= -1) {
+        perror("Could not make the socket a listening type socket");
+        exit(EXIT_FAILURE);
+    }
+
+    cout << "Listening for requests on port " << port << endl;
+
+    return sock;
+}
+
+int main() 
+{
+    int port = 4310;
+    int socket = create_server_socket(port);
+    char message[1024];
 
     while(1) {
         cout << "Enter a message: ";
         fgets(message, 100, stdin);
-        send(fd, message, strlen(message), 0);
+        send(socket, message, strlen(message), 0);
         if (strcmp(message,"exit") == 0) {
-            close(fd);
+            close(socket);
             return 0;
         }
         memset(&message,0,sizeof(message));
--- src/server.cpp
+++ src/server.cpp
@@ -6,24 +6,45 @@
 #include <arpa/inet.h>
 #include <signal.h>
 #include <pthread.h>
-#include "utils.h"
 
 using namespace std;
 
 int create_server_socket(int port) {
 
     struct sockaddr_in server_address;
-    
-    int socket = socket(AF_INET,
-                        SOCK_STREAM,
-                        0);
+
+    int opt = 1;
 
     bzero(&server_address, sizeof(server_address));
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = INADDR_ANY;
     server_address.sin_port = htons(port);
+    
+    //Create the socket
+    int sock = socket(AF_INET,
+                        SOCK_STREAM,
+                        0);
 
-    cout << "Listening for requests on port " <<
+    //Check for error with socket
+    if (sock < 0) {
+        perror("Error with socket");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    }
+
+    // Attempt to make the socket (fd) a listening type socket
+    if (listen(sock, 10) <= -1) {
+        perror("Could not make the socket a listening type socket");
+        exit(EXIT_FAILURE);
+    }
+
+    cout << "Listening for requests on port " << port << endl;
+
+    return sock;
 }
 
 void* handle_client(void* client_ptr) {
@@ -31,56 +52,28 @@ void* handle_client(void* client_ptr) {
 
     int client = *((int*) client_ptr);
 
-    char buffer[BUFF_SIZE + 1];
-    bzero(buffer, sizeof(buffer));
-    int bytes_read = recv(client, buffer, sizeof(buffer), 0); 
+
+    //Request from the client
+    char request[BUFSIZ + 1];
+    bzero(request, sizeof(request));
+    int bytes_read = recv(client, request, sizeof(request), 0); 
     if (bytes_read < 0)
-    { 
-        error_msg("Problem with recv call", false);
+    {
+        perror("Error reading from client"); 
     }
+    cout << request << endl;
+    close(client);
+    return 0;
 }
 
 int main() {
-    struct sockaddr_in server;
-    int fd;
-    int portNum = 4310;
-    int opt = 1;
-
-    int connection;
-
-    char message[1024];
-
-    // Populate socket address structure
-    server.sin_family = AF_INET;
-    server.sin_port = htons(portNum);
-    server.sin_addr.s_addr = INADDR_ANY;
-
-    // Attempt to create a socket and assign it to fd
-    if (fd = socket(AF_INET, SOCK_STREAM, 0) < 0) {
-        perror("Error with socket");
-        exit(EXIT_FAILURE);
-    }
-
-    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
-        perror("setsockopt"); 
-        exit(EXIT_FAILURE); 
-    }
-
-    // Attempt to assign the socket address to a socket (fd)
-    if (bind(fd, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        perror("Binding error\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // Attempt to make the socket (fd) a listening type socket
-    if (listen(fd, 10) <= -1) {
-        perror("Could not make the socket a listening type socket");
-        exit(EXIT_FAILURE);
-    }
+    int port = 4310;
+    int socket;
+    socket = create_server_socket(port);
 
     // Loop until calling accept returns -1 or lower value
     while (true) {
-        int client = accept(fd, (struct sockaddr *)NULL, NULL);
+        int client = accept(socket, (struct sockaddr *)NULL, NULL);
 
         pthread_t tid;
         int flag = pthread_create(&tid, NULL, handle_client, &client);
@@ -89,4 +82,6 @@ int main() {
             perror("Problem creating thread");
         }
     }
+
+    return 0;
 }
